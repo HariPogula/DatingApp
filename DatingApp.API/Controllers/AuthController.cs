293747dd5d39dec7b4a327db46d149ee23698100
Hari <hariar2.0@outlook.com>
2020-05-14 09:09:51 -0600

    Added Auth Controller

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,16 +1,23 @@
 using System.Threading.Tasks;
+using DatingApp.API.DTO;
+using DatingApp.API.Repos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
-        public AuthController()
+        private readonly IAuthRepository repo;
+
+        public AuthController(IAuthRepository repo)
         {
+            this.repo = repo;
 
         }
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] UserRegisterDTO userRegisterDTO)
         {
             //TODO: Implement Realistic Implementation
             await Task.Yield();
