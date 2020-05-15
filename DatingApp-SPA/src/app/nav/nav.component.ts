4be8566204d515b2df202b4464ef43a521dfe844
Hari <hariar2.0@outlook.com>
2020-05-14 22:23:57 -0600

    Adding Login functionality

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss'],
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {}
+
+  Login() {
+    console.log('Component model is ' + JSON.stringify(this.model));
+
+    this.authService.Login(this.model).subscribe(
+      (next) => {
+        console.log('Logged in Successfully.');
+      },
+      (err) => {
+        console.log('Something went wrong..');
+      }
+    );
+  }
+}
